40b36ca157359fa714e148b80e26805d4c8a5e05
kamike <751190264@qq.com>
2017-05-08 16:40:21 +0800

    home

---
+++ app/src/main/java/com/pursuege/gether/android/ui/HomeActivity.java
@@ -0,0 +1,22 @@
+package com.pursuege.gether.android.ui;
+
+import android.os.Bundle;
+
+import com.pursuege.gether.android.BaseActivity;
+import com.pursuege.gether.android.R;
+
+public class HomeActivity extends BaseActivity {
+
+
+
+    @Override
+    public void initShowLayout() {
+        setContentView(R.layout.activity_home);
+
+    }
+
+    @Override
+    public void setAllData() {
+
+    }
+}
--- app/src/main/java/com/pursuege/gether/android/ui/MainActivity.java
+++ app/src/main/java/com/pursuege/gether/android/ui/MainActivity.java
@@ -1,6 +1,8 @@
 package com.pursuege.gether.android.ui;
 
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -8,6 +10,18 @@ import com.pursuege.gether.android.BaseActivity;
 import com.pursuege.gether.android.R;
 
 public class MainActivity extends BaseActivity {
+
+    private LinearLayout mMainParamsLinear;
+    private ImageView mHomeMobileIv;
+    private TextView mHomeMobileTv;
+    private ImageView mHomeTaskIv;
+    private TextView mHomeTaskTv;
+    private ImageView mHomeTestIv;
+    private TextView mHomeTestTv;
+    private ImageView mHomeSystemInfoIv;
+    private TextView mHomeSystemInfoTv;
+    private ImageView mHomeLogIv;
+    private TextView mHomeLogTv;
 
     private LinearLayout linearParams;
 
@@ -17,11 +31,30 @@ public class MainActivity extends BaseActivity {
         doSetStatusBars();
         setTranslucent(this);
         linearParams = (LinearLayout) findViewById(R.id.main_params_linear);
+        assignViews();
 
     }
 
+
+    private void assignViews() {
+        mMainParamsLinear = (LinearLayout) findViewById(R.id.main_params_linear);
+        mHomeMobileIv = (ImageView) findViewById(R.id.home_mobile_iv);
+        mHomeMobileTv = (TextView) findViewById(R.id.home_mobile_tv);
+        mHomeTaskIv = (ImageView) findViewById(R.id.home_task_iv);
+        mHomeTaskTv = (TextView) findViewById(R.id.home_task_tv);
+        mHomeTestIv = (ImageView) findViewById(R.id.home_test_iv);
+        mHomeTestTv = (TextView) findViewById(R.id.home_test_tv);
+        mHomeSystemInfoIv = (ImageView) findViewById(R.id.home_system_info_iv);
+        mHomeSystemInfoTv = (TextView) findViewById(R.id.home_system_info_tv);
+        mHomeLogIv = (ImageView) findViewById(R.id.home_log_iv);
+        mHomeLogTv = (TextView) findViewById(R.id.home_log_tv);
+    }
+
+
     @Override
     public void setAllData() {
+        colorBule=getResources().getColor(R.color.color_blue_them);
+        colorHint=getResources().getColor(R.color.color_text_hint);
         addBaseParams();
         addProgressLocaltion();
     }
@@ -66,5 +99,99 @@ public class MainActivity extends BaseActivity {
 
     private void addProgressLocaltion() {
 
+    }
+
+    public void onclickHomeLog(View view) {
+        setHomeIndex(5);
+    }
+
+    public void onclickHomeSystemInfo(View view) {
+        setHomeIndex(4);
+    }
+
+    public void onclickHomeTest(View view) {
+        setHomeIndex(3);
+    }
+
+    public void onclickHomeTask(View view) {
+        setHomeIndex(2);
+    }
+
+    public void onclickHomeMobile(View view) {
+        setHomeIndex(1);
+    }
+    private int colorBule,colorHint;
+
+    private void setHomeIndex(int index) {
+         switch (index) {
+              case 1:
+                  mHomeMobileTv.setTextColor(colorBule);
+                  mHomeTaskTv.setTextColor(colorHint);
+                  mHomeTestTv.setTextColor(colorHint);
+                  mHomeSystemInfoTv.setTextColor(colorHint);
+                  mHomeLogTv.setTextColor(colorHint);
+
+                  mHomeMobileIv.setImageResource(R.drawable.icon_home_mobile_press);
+                  mHomeTaskIv.setImageResource(R.drawable.icon_home_task);
+                  mHomeTestIv.setImageResource(R.drawable.icon_home_test);
+                  mHomeSystemInfoIv.setImageResource(R.drawable.icon_home_system_info);
+                  mHomeLogIv.setImageResource(R.drawable.icon_home_log);
+
+                  break;
+              case 2:
+                  mHomeMobileTv.setTextColor(colorHint);
+                  mHomeTaskTv.setTextColor(colorBule);
+                  mHomeTestTv.setTextColor(colorHint);
+                  mHomeSystemInfoTv.setTextColor(colorHint);
+                  mHomeLogTv.setTextColor(colorHint);
+
+                  mHomeMobileIv.setImageResource(R.drawable.icon_home_mobile);
+                  mHomeTaskIv.setImageResource(R.drawable.icon_home_task_press);
+                  mHomeTestIv.setImageResource(R.drawable.icon_home_test);
+                  mHomeSystemInfoIv.setImageResource(R.drawable.icon_home_system_info);
+                  mHomeLogIv.setImageResource(R.drawable.icon_home_log);
+                  break;
+              case 3:
+                  mHomeMobileTv.setTextColor(colorHint);
+                  mHomeTaskTv.setTextColor(colorHint);
+                  mHomeTestTv.setTextColor(colorBule);
+                  mHomeSystemInfoTv.setTextColor(colorHint);
+                  mHomeLogTv.setTextColor(colorHint);
+
+                  mHomeMobileIv.setImageResource(R.drawable.icon_home_mobile);
+                  mHomeTaskIv.setImageResource(R.drawable.icon_home_task);
+                  mHomeTestIv.setImageResource(R.drawable.icon_home_test_press);
+                  mHomeSystemInfoIv.setImageResource(R.drawable.icon_home_system_info);
+                  mHomeLogIv.setImageResource(R.drawable.icon_home_log);
+
+                  break;
+              case 4:
+                  mHomeMobileTv.setTextColor(colorHint);
+                  mHomeTaskTv.setTextColor(colorHint);
+                  mHomeTestTv.setTextColor(colorHint);
+                  mHomeSystemInfoTv.setTextColor(colorBule);
+                  mHomeLogTv.setTextColor(colorHint);
+
+                  mHomeMobileIv.setImageResource(R.drawable.icon_home_mobile);
+                  mHomeTaskIv.setImageResource(R.drawable.icon_home_task);
+                  mHomeTestIv.setImageResource(R.drawable.icon_home_test);
+                  mHomeSystemInfoIv.setImageResource(R.drawable.icon_home_system_info_press);
+                  mHomeLogIv.setImageResource(R.drawable.icon_home_log);
+                  break;
+             case 5:
+                 mHomeMobileTv.setTextColor(colorHint);
+                 mHomeTaskTv.setTextColor(colorHint);
+                 mHomeTestTv.setTextColor(colorHint);
+                 mHomeSystemInfoTv.setTextColor(colorHint);
+                 mHomeLogTv.setTextColor(colorBule);
+
+                 mHomeMobileIv.setImageResource(R.drawable.icon_home_mobile);
+                 mHomeTaskIv.setImageResource(R.drawable.icon_home_task);
+                 mHomeTestIv.setImageResource(R.drawable.icon_home_test);
+                 mHomeSystemInfoIv.setImageResource(R.drawable.icon_home_system_info);
+                 mHomeLogIv.setImageResource(R.drawable.icon_home_log_press);
+                 break;
+                                 
+          }
     }
 }
